7f75ad23e359ea64a85ce664e99231708cdb77ed
Mintesn <mketema4@gmail.com>
2023-09-18 09:22:50 +0300

    response nav bar

---
+++ src/components/Pages/Home.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "../Pages/home.css"
+
+export const Home = () => {
+  return (
+    <div className="section">
+      <div className="container">
+           
+         <h1 className="lok">Apollo</h1>   
+          
+      </div>
+      <div className="box">
+
+      </div>
+      
+    </div>
+  );
+};
